eb0be1290a59221fd41e5f60f459d23c22d7ca72
PinguMac <z7924723@gmail.com>
2018-03-16 15:07:14 +0800

    Refactoring file

--- Cloudy/Extensions/UIImage.swift
+++ Cloudy/Extensions/UIImage.swift
@@ -12,9 +12,14 @@ extension UIImage {
   
   class func imageForIcon(withName name: String) -> UIImage? {
     switch name {
-    case "clear-day", "clear-night", "rain", "snow", "sleet": return UIImage(named: name)
-    case "wind", "cloudy", "partly-cloudy-day", "partly-cloudy-night": return UIImage(named: "cloudy")
-    default: return UIImage(named: "clear-day")
+    case "clear-day", "clear-night", "rain", "snow", "sleet":
+      return UIImage(named: name)
+      
+    case "wind", "cloudy", "partly-cloudy-day", "partly-cloudy-night":
+      return UIImage(named: "cloudy")
+      
+    default:
+      return UIImage(named: "clear-day")
     }
   }
   
